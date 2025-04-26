fa4e06368cd8895d46152ce1af2b69071dba0852
许金泉 <xujinquan@tuhu.cn>
2025-04-26 09:52:06 +0800

    测试

--- nfine-web/src/main/java/com/nfineweb/NfineWebApplication.java
+++ nfine-web/src/main/java/com/nfineweb/NfineWebApplication.java
@@ -1,8 +1,13 @@
 package com.nfineweb;
 
+import com.sun.org.apache.xpath.internal.objects.XNull;
+import org.hibernate.validator.internal.util.privilegedactions.LoadClass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
+import javax.swing.text.Utilities;
 import java.sql.SQLException;
 
 @SpringBootApplication
@@ -10,5 +15,16 @@ public class NfineWebApplication {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		SpringApplication.run(NfineWebApplication.class, args);
+		// 获取当前程序的类加载器
+		ClassLoader classLoader = SpringApplication.class.getClassLoader();
+		ClassLoader parent = classLoader.getParent().getParent();
+		Class<?> aClass = parent.loadClass("com.mysql.jdbc.Driver");
+		if (aClass == null) {
+			return;
+		}
+		if (StringUtils.isEmpty(aClass.toString())) {
+			return;
+		}
+
 	}
 }
